ab116f122cf2044f243739391e1e9c3dffaf5a85
NKSmagic <schoell.niklas@gmail.com>
2025-05-26 10:43:16 +0200

    Finished CH5 L1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
+
+
+
+def copy_directory_contents(source_dir, dest_dir, is_initial_call = True):
+    if is_initial_call:
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
+        os.makedirs(dest_dir)
+    static = os.listdir(source_dir)
+    for item in static:
+        if os.path.isfile(os.path.join(source_dir, item)):
+            print(f"Copying file: {os.path.join(source_dir, item)}")
+            shutil.copy(os.path.join(source_dir, item), os.path.join(dest_dir, item))
+        else:
+            os.makedirs(os.path.join(dest_dir, item))
+            copy_directory_contents(os.path.join(source_dir, item), os.path.join(dest_dir, item), False)
 
 def main():
     textnode = TextNode("Here is some example text", TextType.BOLD)
     print(textnode)
+    copy_directory_contents("./static", "./public")
 
 if __name__ == "__main__":
     main()
